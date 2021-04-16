a62c0f15e77b91134bec3dd72d38118a103bb133
Pedro Seoane Prado <info@pedroseoaneprado.es>
2021-04-16 19:16:20 +0200

    longterm:	5.10.31

--- source/include/generated/compile.h
+++ source/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 6 */
 /* SMP */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#2 SMP Wed Apr 14 16:33:23 CEST 2021"
+#define UTS_VERSION "#6 SMP Fri Apr 16 19:15:48 CEST 2021"
 #define LINUX_COMPILE_BY "peseoane"
-#define LINUX_COMPILE_HOST "PESEOANE-WS"
+#define LINUX_COMPILE_HOST "PESEOANE-TB"
 #define LINUX_COMPILER "gcc (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0, GNU ld (GNU Binutils for Ubuntu) 2.34"
--- source/include/generated/utsrelease.h
+++ source/include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "5.10.30{Custom WSL2 kernel by peseoane}"
+#define UTS_RELEASE "5.10.31"
