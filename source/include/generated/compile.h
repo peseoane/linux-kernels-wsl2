13dc758e60cd18c2925f5ec8c07018b391cfb37e
Pedro Seoane Prado <info@pedroseoaneprado.es>
2021-04-29 08:23:32 +0200

    longterm: 5.10.33-NT

--- source/include/generated/compile.h
+++ source/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 9 */
+/* This file is auto generated, version 11 */
 /* SMP */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#9 SMP Wed Apr 21 23:57:43 CEST 2021"
+#define UTS_VERSION "#11 SMP Thu Apr 29 08:22:33 CEST 2021"
 #define LINUX_COMPILE_BY "peseoane"
 #define LINUX_COMPILE_HOST "PESEOANE-TB"
 #define LINUX_COMPILER "gcc (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0, GNU ld (GNU Binutils for Ubuntu) 2.34"
