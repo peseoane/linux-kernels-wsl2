f2830dd59cc285161a934c198383d3c966161769
Pedro Seoane Prado <info@pedroseoaneprado.es>
2021-06-10 17:48:36 +0200

    stable: 5.12.10

--- source/include/generated/compile.h
+++ source/include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 2 */
 /* SMP */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#2 SMP Tue Jun 8 20:34:29 CEST 2021"
+#define UTS_VERSION "#2 SMP Thu Jun 10 17:45:49 CEST 2021"
 #define LINUX_COMPILE_BY "peseoane"
-#define LINUX_COMPILE_HOST "PESEOANE-WS"
+#define LINUX_COMPILE_HOST "PESEOANE-TB"
 #define LINUX_COMPILER "gcc (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0, GNU ld (GNU Binutils for Ubuntu) 2.34"
